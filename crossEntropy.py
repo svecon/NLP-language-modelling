4616ae0aa7d26ef5f251b49da3995e61e1896ce1
Ondřej Švec <ond.svec@gmail.com>
2016-02-27 12:20:17 +0100

    Added comments to source codes; fixed conditional probablities; added coverage statistics

--- crossEntropy.py
+++ crossEntropy.py
@@ -1,142 +1,242 @@
+# Calculating cross entropy of a text
+# Program expects first argument to be filename of the text file to be examined.
+
+### @@@TODO
+
+# @AUTHOR: Ondřej Švec
+
 import sys
 import random
 import operator
 import functools
 from math import log
 
-TEST_DATA_COUNT = 20000
-HELDOUT_DATA_COUNT = 40000
-DATA_ENCODING = "iso-8859-2"
+TEST_DATA_COUNT = 20000  # number of words for test data
+HELDOUT_DATA_COUNT = 40000  # number of words for heldout data
 
-data = [line.strip() for line in open(sys.argv[1], 'r', encoding=DATA_ENCODING)]
+data = [line.strip() for line in open(sys.argv[1], 'r', encoding="iso-8859-2")]
 
-testData = data[-TEST_DATA_COUNT:]
-del data[-TEST_DATA_COUNT:]
+testData = data[-TEST_DATA_COUNT:]  # get testData from data
+del data[-TEST_DATA_COUNT:]  # remove them from data
 
-heldoutData = data[-HELDOUT_DATA_COUNT:]
-del data[-HELDOUT_DATA_COUNT:]
+heldoutData = data[-HELDOUT_DATA_COUNT:]  # get heldout data from data
+del data[-HELDOUT_DATA_COUNT:]  # remove them from data
 
+# getting statistics from the data:
+trainingDataStats = {}
+heldoutDataStats = {}
+testDataStats = {}
+
+for w in data:
+    trainingDataStats[w] = 1 if w not in trainingDataStats else trainingDataStats[w] + 1
+for w in heldoutData:
+    heldoutDataStats[w] = 1 if w not in heldoutDataStats else heldoutDataStats[w] + 1
+for w in testData:
+    testDataStats[w] = 1 if w not in testDataStats else testDataStats[w] + 1
+print('Training data stats:')
+print('Words in text: ', len(data))
+print('Unique words: ', len(trainingDataStats))
+print('Frequency of most common: ', max(trainingDataStats.items(), key=operator.itemgetter(1))[1])
+print('Number of words with 1 occurence: ',
+      functools.reduce(lambda count, x: count + (x == 1), trainingDataStats.values(), 0))
+print()
+
+print('Heldout data stats:')
+print('Words in text: ', len(heldoutData))
+print('Unique words: ', len(heldoutDataStats))
+print('Frequency of most common: ', max(heldoutDataStats.items(), key=operator.itemgetter(1))[1])
+print('Number of words with 1 occurence: ',
+      functools.reduce(lambda count, x: count + (x == 1), heldoutDataStats.values(), 0))
+print('Coverage of unique heldout data: ',
+      sum([1 if w in trainingDataStats else 0 for w in heldoutDataStats.keys()]) / len(heldoutDataStats.keys()))
+print('Coverage of heldout data: ',
+      sum([1 if w in trainingDataStats else 0 for w in heldoutData]) / HELDOUT_DATA_COUNT)
+print()
+
+print('Test data stats:')
+print('Words in text: ', len(testData))
+print('Unique words: ', len(testDataStats))
+print('Frequency of most common: ', max(testDataStats.items(), key=operator.itemgetter(1))[1])
+print('Number of words with 1 occurence: ',
+      functools.reduce(lambda count, x: count + (x == 1), testDataStats.values(), 0))
+print('Coverage of unique test data: ',
+      sum([1 if w in trainingDataStats else 0 for w in testDataStats.keys()]) / len(testDataStats.keys()))
+print('Coverage of test data: ',
+      sum([1 if w in trainingDataStats else 0 for w in testData]) / TEST_DATA_COUNT)
+print()
+
+# dictionaries for unigram, bigram and trigram counts
 unigram = {}
 bigram = {}
 trigram = {}
 
 wh2 = "<<s>>"  # word history 2
 wh1 = "<s>"  # word history 1
-for w in data:
+for w in data:  # iterate over data
+    # +1 for the word in unigram dictionary
     unigram[w] = 1 if w not in unigram else unigram[w] + 1
 
+    # add history to bigram if not there yet
     if wh1 not in bigram: bigram[wh1] = {}
+    # +1 for the word in bigram dictionary
     bigram[wh1][w] = 1 if w not in bigram[wh1] else bigram[wh1][w] + 1
 
+    # add history to trigram if not there yet
     if wh2 not in trigram: trigram[wh2] = {}
     if wh1 not in trigram[wh2]: trigram[wh2][wh1] = {}
+    # +1 for the word in trigram dictionary
     trigram[wh2][wh1][w] = 1 if w not in trigram[wh2][wh1] else trigram[wh2][wh1][w] + 1
 
+    # move histories
     wh2 = wh1
     wh1 = w
 
-vocabularySize = len(unigram)
-textSize = len(data)
+vocabularySize = len(unigram)  # number of unique words
+textSize = len(data)  # number of all words
 
-def flatProbConditional():
-    return 1 / vocabularySize
-
-
-def unigramProbConditional(word):
+# Helper function: returns 0 if not in unigram dictionary, otherwise returns unigram count
+def unigramCount(word):
     if word not in unigram:
         return 0
 
-    return unigram[word] / textSize
+    return unigram[word]
 
-
-def bigramProbConditional(word, h1):
+# Helper function: returns 0 if not in bigram dictionary, otherwise returns bigram count
+def bigramCount(word, h1):
     if (h1 not in bigram) or (word not in bigram[h1]):
         return 0
 
-    if h1 not in unigram:
-        return 0
+    return bigram[h1][word]
 
-    return bigram[h1][word] / unigram[h1]
-
-
-def trigramProbConditional(word, h1, h2):
+# Helper function: returns 0 if not in trigram dictionary, otherwise returns trigram count
+def trigramCount(word, h1, h2):
     if (h2 not in trigram) or (h1 not in trigram[h2]) or (word not in trigram[h2][h1]):
         return 0
 
-    if (h2 not in bigram) or (h1 not in bigram[h2]):
-        return 0
+    return trigram[h2][h1][word]
 
-    return trigram[h2][h1][word] / bigram[h2][h1] #sum(trigram[h2][h1].values())
+# returns 0 if denominator is 0, otherwise performs division
+def divisionOrZero(nominator, denominator):
+    if denominator == 0: return 0
+    return nominator / denominator
+
+# Calculates uniform conditional probability (p0)
+def uniformProbConditional():
+    return 1 / vocabularySize
 
 
+# Calculates unigram conditional probability (p1)
+def unigramProbConditional(word):
+    return unigramCount(word) / textSize
+
+
+# Calculates bigram conditional probability (p2)
+def bigramProbConditional(word, h1):
+    # use uniform conditional probability when both bigram and unigram counts are zero
+    # @why? because the scripts says so
+    if bigramCount(word, h1) == 0:
+        if unigramCount(h1) == 0:
+            return uniformProbConditional()
+
+    return divisionOrZero(bigramCount(word, h1), unigramCount(h1))
+
+
+# Calculates trigram conditional probability (p3)
+def trigramProbConditional(word, h1, h2):
+    # use uniform conditional probability when both bigram and unigram counts are zero
+    # @why? because the scripts says so
+    if trigramCount(word, h1, h2) == 0:
+        if bigramCount(h1, h2) == 0:
+            return uniformProbConditional()
+
+    return divisionOrZero(trigramCount(word, h1, h2), bigramCount(h1, h2))
+
+
+# Calculates smoothed conditional probability
+# using linear combination of lambdas and uniform, unigram, bigram and trigram conditional probabilities
 def smoothedProbConditional(word, h1, h2, lambdas):
-    return   lambdas[0] * flatProbConditional() \
+    return lambdas[0] * uniformProbConditional() \
            + lambdas[1] * unigramProbConditional(word) \
            + lambdas[2] * bigramProbConditional(word, h1) \
            + lambdas[3] * trigramProbConditional(word, h1, h2)
 
 
-# print(flatProbConditional())
-# print(unigramProbConditional('ale'))
-# print(bigramProbConditional('ale', ','))
-# print(trigramProbConditional('ale', ',', 'situace'))
+### EM ALGORITHM ###
 
-# EM ALGORITHM
-lambdas = [.7,.1,.1,.1] #[1/4]*4
-while False:
-    expCounts = [0]*4
+# initialize lambdas
+lambdas = [1 / 4] * 4
+while True:
+    expCounts = [0] * 4  # expected counts for lambdas
     wh2 = "<<s>>"  # word history 2
     wh1 = "<s>"  # word history 1
-    for w in data:
-        # print("{:s} {:s} {:s} {:.6e} {:.6e} {:.6e}".format(wh2, wh1, w, unigramProbConditional(w), bigramProbConditional(w, wh1), trigramProbConditional(w, wh1, wh2)))
-        expCounts[0] += lambdas[0]*flatProbConditional()/smoothedProbConditional(w,wh1,wh2,lambdas)
-        expCounts[1] += lambdas[1]*unigramProbConditional(w)/smoothedProbConditional(w,wh1,wh2,lambdas)
-        expCounts[2] += lambdas[2]*bigramProbConditional(w,wh1)/smoothedProbConditional(w,wh1,wh2,lambdas)
-        expCounts[3] += lambdas[3]*trigramProbConditional(w,wh1,wh2)/smoothedProbConditional(w,wh1,wh2,lambdas)
 
+    # train lambdas using the heldoutData
+    for w in heldoutData:
+        # calculate expected counts for each lambda using uniform, unigrams, bigrams, trigrams
+        expCounts[0] += lambdas[0] * uniformProbConditional() / smoothedProbConditional(w, wh1, wh2, lambdas)
+        expCounts[1] += lambdas[1] * unigramProbConditional(w) / smoothedProbConditional(w, wh1, wh2, lambdas)
+        expCounts[2] += lambdas[2] * bigramProbConditional(w, wh1) / smoothedProbConditional(w, wh1, wh2, lambdas)
+        expCounts[3] += lambdas[3] * trigramProbConditional(w, wh1, wh2) / smoothedProbConditional(w, wh1, wh2, lambdas)
+
+        # move histories
         wh2 = wh1
         wh1 = w
 
+    # get new lambdas by normalizing expected counts
     newLambdas = list(map(lambda x: x / sum(expCounts), expCounts))
 
-    if all( [ abs(x-y) < 0.0001 for x,y in zip(newLambdas, lambdas) ] ): break
+    # terminate if all lambdas differ by less than 0.0001
+    if all([abs(x - y) < 0.0001 for x, y in zip(newLambdas, lambdas)]): break
 
+    # use new lambdas for the next cycle
     lambdas = newLambdas
 
-lambdasEN = [0.09843454910828085, 0.26415213029970314, 0.5077064080458099, 0.1297069125462061];
-lambdas = lambdasEN
 
+# Modifies the last lambda by difference
+# and normalizes the rest of the lambdas to that they sum up to 1
 def modifyLambdas(lambdas, difference):
     newLambdas = list(map(lambda x: x - difference * x / sum(lambdas[0:3]), lambdas))
     newLambdas[3] = lambdas[3] + difference
     return newLambdas
 
+
+# Boosts the last lambda by percentage
 def boostLambdas(lambdas, percentage):
     return modifyLambdas(lambdas, (1 - lambdas[3]) * percentage)
 
-boostLambdas([0.140, 0.429, 0.245, 0.186 ], 0.1)
 
+# Discounts the last lambda by percentage
 def discountLambdas(lambdas, percentage):
     return modifyLambdas(lambdas, (-1) * lambdas[3] * (1 - percentage))
 
-discountLambdas([0.140, 0.429, 0.245, 0.186 ], .9)
 
-boostingVector = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9, .95, .99]
-discountVector = [.9, .8, .7, .6, .5, .4, .3, .2, .1, 0]
+# Lists of percentages that we want to use for boosting and discounting
+discountVector = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+boostingVector = [   .1, .2, .3, .4, .5, .6, .7, .8, .9, .95, .99]
 
+
+# Calculates cross entropy on the testData using given lambdas
 def calculateCrossEntropy(lambdas):
-    wh2 = "<<s>>" # word history 2
-    wh1 = "<s>" # word history 1
+    wh2 = "<<s>>"  # word history 2
+    wh1 = "<s>"  # word history 1
     crossEntropy = 0
     for w in testData:
         crossEntropy -= log(smoothedProbConditional(w, wh1, wh2, lambdas), 2)
 
+        # move histories
         wh2 = wh1
         wh1 = w
-    return crossEntropy / len(testData)
+    return crossEntropy / len(testData)  # we need to normalize by the length of the test data
 
-for b in boostingVector:
-    print('boosting by ', b, 'entropy', calculateCrossEntropy(boostLambdas(lambdas, b)), 'lambdas', boostLambdas(lambdas, b))
-
+# discount lambdas by some factor and calculate cross entropy
 for b in discountVector:
-    print('discount to ', b, 'entropy', calculateCrossEntropy(discountLambdas(lambdas, b)), 'lambdas', discountLambdas(lambdas, b))
+    print('discount to ', b, 'entropy', calculateCrossEntropy(discountLambdas(lambdas, b)), 'lambdas',
+          discountLambdas(lambdas, b))
+
+# calculate cross entropy for orignal lambdas too
+print('original lambdas entropy', calculateCrossEntropy(lambdas), 'lambdas', lambdas)
+
+# boost lambdas by some factor and calculate cross entropy
+for b in boostingVector:
+    print('boosting by ', b, 'entropy', calculateCrossEntropy(boostLambdas(lambdas, b)), 'lambdas',
+          boostLambdas(lambdas, b))
--- entropy.py
+++ entropy.py
@@ -1,13 +1,24 @@
+# Calculating conditional entropy of a text
+# Program expects first argument to be filename of the text file to be examined.
+
+# The script iterates over the text file and messes up the words or characters.
+# The script then calculates counts c(i,j), which is a number of bigrams ij.
+# And stores this in biGram dictionary.
+
+# Finally it iterates over the biGram dicrionary and calculates conditional entropy.
+
+# @AUTHOR: Ondřej Švec
+
 import sys
 import random
 import operator
 import functools
 from math import log
 
-EXPERIMENT_COUNT = 10
-FIRST_WORD_PADDING = "<s>"
+EXPERIMENT_COUNT = 10 # number of repetitions for one messup probability
+FIRST_WORD_PADDING = "<s>" # word used as a beginning of a text
 
-
+# P(word | h1): calculates conditional bigram probability
 def biGramProbConditional(word, h1):
     if (h1 not in biGram) or (word not in biGram[h1]):
         return 0
@@ -17,14 +28,14 @@ def biGramProbConditional(word, h1):
 
     return biGram[h1][word] / uniGram[h1]
 
-
+# P(h1, word): calculates joint bigram probability
 def biGramProb(word, h1):
     if (h1 not in biGram) or (word not in biGram[h1]):
         return 0
 
     return biGram[h1][word] / len(data)
 
-
+# calculates conditional entropy of dataSet using biGram dictionary
 def calculateEntropy(dataSet):
     entropy = 0
     for h1 in biGram.keys():
@@ -34,46 +45,56 @@ def calculateEntropy(dataSet):
     return entropy
 
 
-DATA_ENCODING = "iso-8859-2"
-data = [line.strip() for line in open(sys.argv[1], 'r', encoding=DATA_ENCODING)]
+# read text file and save lines as array to data
+data = [line.strip() for line in open(sys.argv[1], 'r', encoding="iso-8859-2")]
 
-characters = {}
-words = {}
+characters = {} # dictionary of characters that appear in the text file
+words = {} # dictionary of words that appear in the text file
 
+# array of messup probabilities (probability with which the word or character is messed up)
 messUpLikelihoods = [0, .00001, .0001, .001, .01, .05, .1, .2, .3 , .4, .5, .6, .7, .8, .9, 1]
 
+# get all unique words and characters from the input text file
 for w in data:
     words[w] = 1 if w not in words else words[w] + 1
     for c in w:
         characters[c] = 1 if c not in characters else characters[c] + 1
 
+# converting dictionaries to lists
 wordsList = list(words.keys())
 charList = list(characters.keys())
 
+# mess up characters (messUpWords==0) and words (messUpWords==1)
 for messUpWords in [0,1]:
     print("=== Messing up characters ===" if messUpWords == 0 else "=== Messing up words ===")
+
+    # iterate over all messup probabilities
     for messUpProb in messUpLikelihoods:
 
+    	# lists in which we store calculated information from individual experiments (each messup probability is run 10 times)
         entropies = []
         wordCounts = []
         characterCounts = []
         biggestFrequencies = []
         noOfFreqOne = []
 
+        # mess the words/characters up 10 times
         for i in range(EXPERIMENT_COUNT):
             if (messUpProb == 0) and i > 0: break # no need to repeat when probability is 0
 
-            uniGram = {}
-            biGram = {}
+            uniGram = {} # dictionary for unigram counts
+            biGram = {} # dictionary for bigram counts
             newDataSet = []
 
             wh1 = FIRST_WORD_PADDING  # word history
-            for w in data:
+            for w in data: # iterate over data (text file)
                 newWord = w
 
+                # get a random word from available words when the messup prob is higher than random
                 if messUpWords:
                     if random.random() <= messUpProb:
                         newWord = random.choice(wordsList)
+                # iterate over a word and get a random character from all available characters if the messup prob is higher then random
                 else:
                     newWord = list(newWord)
                     for il in range(len(newWord)):
@@ -81,21 +102,29 @@ for messUpWords in [0,1]:
                             newWord[il] = random.choice(charList)
                     newWord = ''.join(newWord)
 
+                # +1 for the word in unigram dictionary
                 uniGram[newWord] = 1 if newWord not in uniGram else uniGram[newWord] + 1
 
-                if wh1 not in biGram: biGram[wh1] = {}
+                if wh1 not in biGram: biGram[wh1] = {} # add history to biGram if not there yet
+                # +1 for the word in bigram dictionary
                 biGram[wh1][newWord] = 1 if newWord not in biGram[wh1] else biGram[wh1][newWord] + 1
 
+                # add the messed up word in list to save for later (calculating entropy)
                 newDataSet.append(newWord)
-                wh1 = newWord
+                wh1 = newWord # move history
 
+            # calculate conditional entropy and add it into list
             entropies.append(calculateEntropy(newDataSet))
+            # number of unique words
             wordCounts.append(len(uniGram))
+            # number of characters in the new (messed up) text
             characterCounts.append(sum([ len(x) for x in newDataSet ]))
+            # what is a frequency of a word that is most common?
             biggestFrequencies.append(max(uniGram.items(), key=operator.itemgetter(1))[1])
+            # number of words that only appear once in the text
             noOfFreqOne.append(functools.reduce(lambda count, x: count + (x == 1), uniGram.values(), 0))
 
-
+        # print results of all experiments for one messup probability
         print("Messup: ", messUpProb)
         print("Entropies: ", entropies)
         print("Word count: ", wordCounts)
